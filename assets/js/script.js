5f4c4cac8d724496eeedcb2040d7ee595dfc80f6
Yiseo Kwon <yiseo1122@gmail.com>
2023-07-11 15:32:35 -0500

    feat: Implement screen display and list selection functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
 // Global variables
+let quizWrap = document.querySelector(".quiz-wrap");
 let quizTitle = document.querySelector(".quiz-title");
+let startBtn = document.querySelector("#startBtn");
+let initWrap = document.querySelector(".init-wrap");
 let currentTime = document.querySelector(".current-time");
 let timeLeft = 60;
+let score = 0;
+
 
 // Setting quiz question lists
 let questionLists = [
@@ -31,6 +36,16 @@ let questionLists = [
         correctAnswer: "-javascript"
     }
 ]
+let index = 0;
+let currentQuestion = questionLists[index];
+
+
+// Click the start button, a list of questions displays on the screen
+startBtn.addEventListener("click", function(){
+    initWrap.style.display = "none";
+    displayQuestion();
+    startTimer();
+})
 
 // Start timer
 function startTimer(){
@@ -38,14 +53,73 @@ function startTimer(){
         timeLeft--;
         currentTime.textContent = timeLeft;
 
+        if(timeLeft < 10 && timeLeft > 0){
+            currentTime.textContent = "0" + timeLeft;
+        }
+
         if(timeLeft <= 0){
             clearInterval(timeInterval);
         }
     }, 1000);
 }
 
-startTimer()
-
 // Display question lists on the screen
-function DisplayQuestion(){
+function displayQuestion(){
+    let quizUl = document.querySelector(".quiz-lists");
+    let currentQuestion = questionLists[index];
+
+    quizWrap.style.display = "block";
+    quizTitle.textContent = currentQuestion.question;
+    quizUl.innerHTML = "";
+
+    currentQuestion.examples.forEach(example => {
+        let quizLi = document.createElement("li");
+        let quizBtn = document.createElement("button");
+        
+        quizBtn.classList.add("examples");
+        
+        
+        quizUl.appendChild(quizLi);
+        quizLi.appendChild(quizBtn);
+        quizBtn.textContent = example;
+    });
 }
+
+// When the list button is clicked, show the next question and handle answer selection
+function handleSelectedAnswer(event){
+    let selectTarget = event.target;
+
+    if(selectTarget.matches(".examples")){
+        let selectedAnswer = selectTarget.textContent;
+        let currentQuestion = questionLists[index];
+
+        if(selectedAnswer === currentQuestion.correctAnswer){
+            score += 10;
+            alert("You are correct!");
+        }else{
+            timeLeft -= 10;
+            alert("You are wrong!");
+
+            if(timeLeft < 0) {
+                currentTime.textContent = 0;
+            };
+        }
+        
+        index ++;
+        if(index < questionLists.length){
+            displayQuestion();
+        }else{
+            endQuizChallenge();
+        }
+    }else{
+        return;
+    }
+    
+}
+
+// End the coding quiz challenge
+function endQuizChallenge(){
+    alert("End");
+}
+
+document.querySelector(".quiz-lists").addEventListener("click", handleSelectedAnswer);
